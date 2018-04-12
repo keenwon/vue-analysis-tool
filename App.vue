2f4be32b37735399c59a2dd64d61a499e88b2532
keenwon <semanwmj@gmail.com>
2018-04-13 00:01:05 +0800

    组件添加各种功能以测试

--- App.vue
+++ App.vue
@@ -2,7 +2,19 @@
   <div>
     <p>count: {{count}}</p>
     <p>num: {{num}}</p>
+    <div>
+      <p v-if="!isEdit">
+        countX2: {{countX2}}
+        <a href="javascript:;" @click="isEdit = true">edit</a>
+      </p>
+      <p v-if="isEdit">
+        countX2:
+        <input v-model="countX2" />
+        <a href="javascript:;" @click="isEdit = false">done</a>
+      </p>
+    </div>
     <button @click="addCount()">count + 1</button>
+    <button @click="reset()">reset</button>
   </div>
 </template>
 
@@ -16,6 +28,19 @@
   export default class Com extends Vue {
     count = 0;
     num = 0;
+    isEdit = false;
+
+    get countX2() {
+      return this.count * 2;
+    }
+
+    set countX2(value) {
+      this.count = Math.floor(value / 2);
+    }
+
+    created() {
+      // do nothing
+    }
 
     @Watch('count')
     onCountChange() {
@@ -24,6 +49,10 @@
 
     addCount() {
       this.count++;
+    }
+
+    reset() {
+      this.count = 0;
     }
   }
 </script>
